95a78312f229506394c3f585d516eaf05d9b586e
yokogawadeveloper <ankul.gautam@yokogawa.com>
2023-07-05 21:20:49 +0530

    start approval in login api

--- accounts/views.py
+++ accounts/views.py
@@ -17,6 +17,10 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
     permission_classes = [AllowAny]
 
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+    permission_classes = [AllowAny]
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         try:
@@ -25,138 +29,39 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             return Response({'detail': 'No active account found with the given credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
         if serializer.is_valid():
-            if user is  not None:
-                if ApproverMatrix.objects.filter(primary_approver=user.email).exists():
-                    if MasterRoleMapping.objects.filter(employeeId=user.username).exists():
-                        user_type = MasterRoleMapping.objects.filter(employeeId=user.username).values('roleId')
-                        if user_type is not None:
-                            role_name = MasterRole.objects.filter(roleId=user_type).values('rolename')
-                            if role_name == 'Admin':
-                                return Response({
-                                    'access': serializer.validated_data['access'],
-                                    'refresh': serializer.validated_data['refresh'],
-                                    'is_admin':True,
-                                    'is_dsin':False,
-                                    'is_requester':False,
-                                    'is_approver':True,
-                                    'user_id': user.username,
-                                    'user_name': user.name,
-                                    'user_email': user.email,
-                                })
-                            elif role_name == 'DSIN':
-                                return Response({
-                                    'access': serializer.validated_data['access'],
-                                    'refresh': serializer.validated_data['refresh'],
-                                    'is_admin':False,
-                                    'is_dsin':True,
-                                    'is_requester':False,
-                                    'is_approver':True,
-                                })
-                            else:
-                                return Response({
-                                    'access': serializer.validated_data['access'],
-                                    'refresh': serializer.validated_data['refresh'],
-                                    'is_admin':False,
-                                    'is_dsin':False,
-                                    'is_requester':False,
-                                    'is_approver':True,
-                                    # 'user_id': user.username,
-                                    # 'user_name': user.name,
-                                    # 'user_email': user.email,
-                                })
-                else:
-                    if OrgDepartmentHead.objects.filter(Head=user.username).exists():
-                        if MasterRoleMapping.objects.filter(employeeId=user.username).exists():
-                            user_type = MasterRoleMapping.objects.filter(employeeId=user.username).values('roleId')
-                            if user_type is not None:
-                                role_name = MasterRole.objects.filter(roleId=user_type).values('rolename')
-                                if role_name == 'Admin':
-                                    return Response({
-                                        'access': serializer.validated_data['access'],
-                                        'refresh': serializer.validated_data['refresh'],
-                                        'is_admin':True,
-                                        'is_dsin':False,
-                                        'is_requester':False,
-                                        'is_approver':True,
-                                        # 'user_id': user.username,
-                                        # 'user_name': user.name,
-                                        # 'user_email': user.email,
-                                    })
-                                elif role_name == 'DSIN':
-                                    return Response({
-                                        'access': serializer.validated_data['access'],
-                                        'refresh': serializer.validated_data['refresh'],
-                                        'is_admin':False,
-                                        'is_dsin':True,
-                                        'is_requester':False,
-                                        'is_approver':True,
-                                        # 'user_id': user.username,
-                                        # 'user_name': user.name,
-                                        # 'user_email': user.email,
-                                    })
-                                else:
-                                    return Response({
-                                        'access': serializer.validated_data['access'],
-                                        'refresh': serializer.validated_data['refresh'],
-                                        'is_admin':False,
-                                        'is_dsin':False,
-                                        'is_requester':False,
-                                        'is_approver':True,
-                                        # 'user_id': user.username,
-                                        # 'user_name': user.name,
-                                        # 'user_email': user.email,
-                                    })
-                    else:
-                        if MasterRoleMapping.objects.filter(employeeId=user.username).exists():
-                            user_type = MasterRoleMapping.objects.filter(employeeId=user.username).values('roleId')
-                            if user_type is not None:
-                                role_name = MasterRole.objects.filter(roleId=user_type).values('rolename')
-                                if role_name == 'Admin':
-                                    return Response({
-                                        'access': serializer.validated_data['access'],
-                                        'refresh': serializer.validated_data['refresh'],
-                                        'is_admin':True,
-                                        'is_dsin':False,
-                                        'is_requester':True,
-                                        'is_approver':False,
-                                        # 'user_id': user.username,
-                                        # 'user_name': user.name,
-                                        # 'user_email': user.email,
-                                    })
-                                elif role_name == 'DSIN':
-                                    return Response({
-                                        'access': serializer.validated_data['access'],
-                                        'refresh': serializer.validated_data['refresh'],
-                                        'is_admin':False,
-                                        'is_dsin':True,
-                                        'is_requester':True,
-                                        'is_approver':False,
-                                        # 'user_id': user.username,
-                                        # 'user_name': user.name,
-                                        # 'user_email': user.email,
-                                    })
-                                else:
-                                    return Response({
-                                        'access': serializer.validated_data['access'],
-                                        'refresh': serializer.validated_data['refresh'],
-                                        'is_admin':False,
-                                        'is_dsin':False,
-                                        'is_requester':True,
-                                        'is_approver':False,
-                                        # 'user_id': user.username,
-                                        # 'user_name': user.name,
-                                        # 'user_email': user.email,
-                                    })
-            else:
+            if ApproverMatrix.objects.filter(primary_approver=user.email).exists():
                 return Response({
-                'message': 'Invalid username or password',
-                'data': serializer.errors,
-                }, status=status.HTTP_400_BAD_REQUEST)
-        else:
+                    'message': 'Approver login',
+                    'access': serializer.validated_data['access'],
+                    'refresh': serializer.validated_data['refresh'],
+                    'username': user.username,
+                    'name': user.name,
+                    'is_approver': True,
+                    
+                })
+            else:
+                if OrgDepartmentHead.objects.filter(Head=user.username).exists():
+                    return Response({
+                        'message': 'Department Head login',
+                        'access': serializer.validated_data['access'],
+                        'refresh': serializer.validated_data['refresh'],
+                        'username': user.username,
+                        'name': user.name,
+                        'is_approver': True,
+                        
+                    })
             return Response({
-                'message': 'Invalid username or password',
-                'data': serializer.errors,
-            }, status=status.HTTP_400_BAD_REQUEST)
+                'message': 'Normal User login',
+                'access': serializer.validated_data['access'],
+                'refresh': serializer.validated_data['refresh'],
+                'username': user.username,
+                'name': user.name,
+                'is_approver': False,
+                
+            })
+        else:
+            return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+           
 
 
 class EmployeeUserAPIView(generics.RetrieveAPIView):
